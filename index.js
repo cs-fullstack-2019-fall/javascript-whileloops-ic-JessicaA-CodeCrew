9fcfd6d88bd17e7a8103d2efc579ec2a28398613
JessicaA-CodeCrew <ms.jessica.avarie@gmail.com>
2019-08-08 15:28:52 -0500

    WhileLoops

---
+++ index.js
@@ -0,0 +1,16 @@
+// var currentCount = 10;
+//
+// while (currentCount >= 0)
+// {
+//     console.log(currentCount);
+//     currentCount--;
+// }
+//
+// console.log("LiftOff!!!");
+
+var count = 0
+while(count < 100)
+{
+    count=count+2
+    console.log(count);
+}
